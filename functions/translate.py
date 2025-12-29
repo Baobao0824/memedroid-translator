d9782e756d7d492c15b3ac656457f89b1fa39b04
Baobao0824 <ediamonds233@gmail.com>
2025-12-29 15:57:24 +0800

    完成了从oss中下载图片并翻译

--- functions/translate.py
+++ functions/translate.py
@@ -68,8 +68,7 @@ async def save_image_local(base64_str: str, origin_path: Path) -> None:
 
 async def translate_one_from_oss(key:str) -> None:
     """
-    translate_one 的 翻译一张图片
-    TODO: 直接从阿里云OSS下载图片进行翻译。因为有道传的是base64，因此应该可以直接把字节转成base64传过去。
+    translate_one 翻译一张图片
     :param key: 图片在OSS中的key
     :type key: str
     """
@@ -81,29 +80,32 @@ async def translate_one_from_oss(key:str) -> None:
         response = await OSS_CLIENT.get_object(get_request)
         if not response.body:
             raise Exception(f"Failed to download image from OSS: {key}")
-        with response.body as body_stream:
-
-        image_bytes = response.body.read()
-        base64_str = base64.b64encode(image_bytes)
-
-
-
-    base64_str = youdao_utils.readFileAsBase64(path=path)
-    data = {"q": base64_str, "from": "auto", "to": "auto", "render": "1", "type": "1"}
-    httpx.post(
-        URL,
-    )
-    youdao_utils.addAuthParams(APP_ID, APP_SECRET, data)
-    async with httpx.AsyncClient() as client:
-        r = await client.post(
-            url=URL,
-            data=data,
-            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        # FIXME: 这里的问题不知道怎么修复，但是代码本身是没问题的
+        image_bytes = await response.body.read() #type: ignore
+        if isinstance(image_bytes,bytes):
+            base64_str = base64.b64encode(image_bytes).decode()
+        else:
+            raise Exception(f"Failed to change b64 : {key}")
+        print(base64_str)
+        data = {"q": base64_str, "from": "auto", "to": "auto", "render": "1", "type": "1"}
+        httpx.post(
+            URL
         )
-        r.raise_for_status()
-        response_obj = r.json()
-        # TODO: 存储到阿里云OSS的判定
-        await save_image_local(response_obj["render_image"], Path(key))
+        youdao_utils.addAuthParams(APP_ID, APP_SECRET, data)
+        async with httpx.AsyncClient() as client:
+            r = await client.post(
+                url=URL,
+                data=data,
+                headers={"Content-Type": "application/x-www-form-urlencoded"},
+            )
+            r.raise_for_status()
+            response_obj = r.json()
+            # TODO: 存储到阿里云OSS的判定
+            await save_image_local(response_obj["render_image"], Path(key))
+    except Exception as e:
+        print(e)
+    finally:
+        await OSS_CLIENT.close()
 
 async def get_en_list_from_oss() ->List[str] :
     """
@@ -114,6 +116,7 @@ async def get_en_list_from_oss() ->List[str] :
         continuation_token = None
         get_objects_request = oss.ListObjectsV2Request(
             bucket=CONFIG["oss"]["bucket_name"],
+            # 这里其实写死也没事，毕竟阿里云oss用的就是'/'，如果你用Path的话，在win上面反而会拼错
             prefix=CONFIG["crawler"]["save_path"] + "/",
             max_keys=CONFIG["translate"]["max_key_length"],
             continuation_token=continuation_token,
@@ -136,9 +139,10 @@ async def get_en_list_from_oss() ->List[str] :
 async def translate_all() -> None:
     """
     翻译INPUT_DIR下面的所有图片
-    TODO: 从阿里云OSS容器中提取，然后改造这一段
+    TODO: 从阿里云OSS容器中提取，然后改造这一段。翻译已经写完了，之后就可以写这边了
     """
-    await get_en_list_from_oss()
+    # await get_en_list_from_oss()
+    await translate_one_from_oss('memes_en/ItemSprite_diamond.png')
 
     # finished_images = {p.name for p in OUTPUT_DIR.iterdir()}
     # image_files = [p for p in INPUT_DIR.iterdir() if p.name not in finished_images]
